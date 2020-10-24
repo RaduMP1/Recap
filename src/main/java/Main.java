5bdfb89bfa8cb1d50c34e7ef0d2075bb7ff8fc45
Radu Popescu <mradupopescu@yahoo.com>
2020-10-24 09:45:23 +0300

    linie noua in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Radu");
         System.out.println("some text");
+        System.out.println("linie noua in main");
     }
 }
