cf5e76b121859d22e3c60bed9a09697067661ebe
Radu Popescu <mradupopescu@yahoo.com>
2020-10-29 22:09:30 +0200

    java fundamentals: static context

--- src/main/java/fundamentals/Dog.java
+++ src/main/java/fundamentals/Dog.java
@@ -1,4 +1,12 @@
 package fundamentals;
 
 public class Dog {
+
+    int age;
+
+    public void printAge(){
+//        static int n;   << nu poate fi static in cadrul metodei
+
+        System.out.println(this.age);
+    }
 }
